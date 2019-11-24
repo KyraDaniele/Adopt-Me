d24bf745b8e4ddc091953875cde1f53013c19efd
Kyra Mellema <kemellema@gmail.com>
2019-11-23 19:41:18 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+const Pet = ({ name, animal, breed }) => {
+	return React.createElement("div", {}, [
+		React.createElement("h1", {}, name),
+		React.createElement("h2", {}, animal),
+		React.createElement("h2", {}, breed)
+	]);
+};
+
+const App = () => {
+	return React.createElement("div", { id: "something-important" }, [
+		React.createElement("h1", {}, "Adopt Me!"),
+		React.createElement(Pet, {
+			name: "Zelda",
+			animal: "Cat",
+			breed: "Striped"
+		}),
+		React.createElement(Pet, {
+			name: "Stormageddon, Dark Lord Of All",
+			animal: "Cat",
+			breed: "Blue Cream Tortie"
+		}),
+		React.createElement(Pet, {
+			name: "Tiger Lily",
+			animal: "Cat",
+			breed: "Egyptian Mau"
+		})
+	]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
