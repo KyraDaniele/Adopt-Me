de77e3bc5614d7ad37db775f1f6f0664fb57eec4
Kyra Mellema <kemellema@gmail.com>
2019-12-04 17:18:47 -0500

    Started working with Hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-	return React.createElement("div", { id: "something-important" }, [
-		React.createElement("h1", {}, "Adopt Me!"),
-		React.createElement(Pet, {
-			name: "Zelda",
-			animal: "Cat",
-			breed: "Striped"
-		}),
-		React.createElement(Pet, {
-			name: "Stormageddon, Dark Lord Of All",
-			animal: "Cat",
-			breed: "Blue Cream Tortie"
-		}),
-		React.createElement(Pet, {
-			name: "Steve",
-			animal: "Dog",
-			breed: "Corgi"
-		})
-	]);
+	return (
+		<div>
+			<h1 id="something-important">Adopt Me!</h1>
+			<SearchParams />
+		</div>
+	);
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
